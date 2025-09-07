89e2f945d3d2bfc5bc8f68010d05acb2bc3d3502
Mohamed Moughamir <moughamir@gmail.com>
2025-09-07 20:05:27 +0100

    refactor: consolidate package exports using barrel files

--- packages/shared/lib/shared.dart
+++ packages/shared/lib/shared.dart
@@ -5,31 +5,16 @@
 library shared;
 
 // Domain exports
-export 'domain/entities/entity.dart';
-export 'domain/repositories/generic_repository.dart';
-export 'domain/models/models.dart';
-export 'domain/enums/enums.dart' hide UserRole, BookingStatus;
+export 'domain/domain.dart';
 
 // Data layer exports
-export 'data/mappers/supabase_mappers.dart';
+export 'data/data.dart';
 
 // Utils exports
-export 'utils/extensions/datetime_extensions.dart';
-export 'utils/extensions/string_extensions.dart';
-export 'utils/failures/failure.dart';
-export 'utils/type_defs.dart';
-export 'utils/markdown_utils.dart';
+export 'utils/utils.dart';
 
 // Widget exports
-export 'widgets/buttons/primary_button.dart';
-export 'widgets/containers/info_card.dart';
-export 'widgets/dialogs/generic_dialog.dart';
-export 'widgets/indicators/error_display.dart';
-export 'widgets/indicators/loading_indicator.dart';
-export 'widgets/layout/page_scaffold.dart';
-export 'widgets/layout/responsive_layout.dart';
-export 'widgets/lists/responsive_grid_view.dart';
-export 'widgets/content/markdown_widget.dart';
+export 'widgets/widgets.dart';
 
 // Redux exports
 export 'redux/redux.dart';
